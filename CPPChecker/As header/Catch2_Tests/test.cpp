48f678ad8f9cbb0c14a6dc67385be5f2ba7a3140
nguyen_vh <137927806+nguyen-vh@users.noreply.github.com>
2023-08-03 23:59:12 +0200

    +

--- CPPChecker/As header/Catch2_Tests/test.cpp
+++ CPPChecker/As header/Catch2_Tests/test.cpp
@@ -1,1 +1,13 @@
+#define CATCH_CONFIG_MAIN 
+#include <catch2/catch.hpp> 
 
+// test
+int add(int a, int b) {
+    return a + b;
+}
+
+TEST_CASE("Addition Test") {
+    REQUIRE(add(2, 3) == 5);
+    REQUIRE(add(-2, 5) == 3);
+    REQUIRE(add(0, 0) == 0);
+}

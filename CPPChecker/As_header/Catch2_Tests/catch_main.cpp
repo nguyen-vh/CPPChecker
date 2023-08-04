98b99423f73e6cd2a26a73e8dbdff7e1bacb2cd2
nguyen-vh <137927806+nguyen-vh@users.noreply.github.com>
2023-08-04 12:28:50 +0200

    +

--- CPPChecker/As_header/Catch2_Tests/catch_main.cpp
+++ CPPChecker/As_header/Catch2_Tests/catch_main.cpp
@@ -193,7 +193,7 @@ namespace STUDENT::TASK {
 
     TEST_CASE( "Check Filler" ) {
         CHECK( check_Filler<Filler>( ) == true );
-        CHECK( check_free_function_Filler<>( ) == true );
+        CHECK_FALSE( check_free_function_Filler<>( ) == true );
         CHECK_FALSE( check_free_variable_Filler<>( ) == true );
         }
 
